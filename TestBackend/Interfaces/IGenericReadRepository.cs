295a33d986bc781f07aff06041c6a63488bc153c
Riri-Inferno <takayo.uenter36@icloud.com>
2024-09-29 12:05:15 +0900

    GenericRepositoryの実装

--- TestBackend/Interfaces/IGenericReadRepository.cs
+++ TestBackend/Interfaces/IGenericReadRepository.cs
@@ -1,1 +1,11 @@
-namespace TestBackend.Interfaces;
+using System.Threading.Tasks;
+
+namespace TestBackend.Interfaces
+{
+    public interface IGenericWriteRepository<T> where T : class
+    {
+        Task AddAsync(T entity); // 新規追加
+        Task UpdateAsync(T entity); // 更新
+        Task DeleteAsync(int id); // 削除
+    }
+}
--- TestBackend/Interfaces/IGenericWriteRepository.cs
+++ TestBackend/Interfaces/IGenericWriteRepository.cs
@@ -1,1 +1,12 @@
-namespace TestBackend.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TestBackend.Interfaces
+{
+    public interface IGenericReadRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(); // 全件取得
+        Task<T> GetByIdAsync(int id); // IDによる取得
+        Task<IEnumerable<T>> FindAsync(Func<T, bool> predicate); // 条件による取得
+    }
+}
