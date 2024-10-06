13d8805b40ebc4e88319ea067ad081fbd0171770
Riri-Inferno <takayo.uenter36@icloud.com>
2024-10-06 21:12:00 +0900

    ジェネリックリポジトリにコメントを柄

--- TestBackend/Interfaces/IGenericWriteRepository.cs
+++ TestBackend/Interfaces/IGenericWriteRepository.cs
@@ -7,6 +7,7 @@ namespace TestBackend.Interfaces
         Task AddAsync(T entity); // 新規追加
         Task<T> UpdateAsync(T entity); // 更新
         Task DeleteAsync(int id); // 削除
+        // ToDo: 現在はIDでのみ検索できるが将来的に可変にしたい
         Task<T> UpsertAsync(T entity, int id); // 更新追加
         Task SaveChangesAsync(); // 変更を保存
     }
