3d5d65ee1f81aa915147dfb399f660a21bbf10a8
Riri-Inferno <takayo.uenter36@icloud.com>
2024-10-06 19:58:43 +0900

    ⚡️ジェネリックリポジトリにUpser追加

--- TestBackend/Interfaces/IGenericWriteRepository.cs
+++ TestBackend/Interfaces/IGenericWriteRepository.cs
@@ -7,6 +7,7 @@ namespace TestBackend.Interfaces
         Task AddAsync(T entity); // 新規追加
         Task<T> UpdateAsync(T entity); // 更新
         Task DeleteAsync(int id); // 削除
+        Task<T> UpsertAsync(T entity, int id); // 更新追加
         Task SaveChangesAsync(); // 変更を保存
     }
 }
